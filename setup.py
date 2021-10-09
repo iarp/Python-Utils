1dc0764031739a06de6c8e860e6d5ec47e5c88c3
iarp <iarp.opensource@gmail.com>
2021-10-09 18:02:06 -0400

    fix(testing): incorrect python image selected for 3.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='iarp_utils',
-    version='1.0.2',
+    version='1.1.0',
     description='A personal collection of common python utilities used in various projects',
     url='https://bitbucket.org/iarp/iarp-python-utils/',
     author='IARP',
