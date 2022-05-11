0cda99c5c3c1b8eb0ed2a791ce964ab242440115
iarp <iarp.opensource@gmail.com>
2022-05-11 11:46:48 -0400

    feat(datetimes): add function to convert weekday name to integer

--- iarp_utils/datetimes.py
+++ iarp_utils/datetimes.py
@@ -138,3 +138,23 @@ def fromisoformat(date_string):
         time_components = [0, 0, 0, 0, None]
 
     return datetime(*(date_components + time_components))
+
+
+# The following comes from django-celery-beat
+DAYNAMES = 'sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'
+WEEKDAYS = dict(zip(DAYNAMES, range(7)))
+
+
+def weekday(name):
+    """Return the position of a weekday: 0 - 7, where 0 is Sunday.
+
+    Example:
+        >>> weekday('sunday'), weekday('sun'), weekday('mon')
+        (0, 0, 1)
+    """
+    abbreviation = name[0:3].lower()
+    try:
+        return WEEKDAYS[abbreviation]
+    except KeyError:
+        # Show original day name in exception, instead of abbr.
+        raise KeyError(name)
