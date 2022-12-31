f4977afd477901407388a4859cb987c67bb7fc72
iarp <iarp.opensource@gmail.com>
2022-12-31 18:06:00 -0500

    feat(browsers.mixins): add firefox browser mixins for print to pdf, disable geo,

---
+++ iarp_utils/browser/driver_mixins.py
@@ -0,0 +1,25 @@
+
+class FirefoxPrintToPDFMixin:
+
+    def get_driver_options(self):
+        opts = super().get_driver_options()
+
+        # Print to PDF options
+        opts.set_preference("print.printer_Mozilla_Save_to_PDF.print_to_file", True)
+        opts.set_preference("print_printer", "Mozilla Save to PDF")
+        opts.set_preference("print.always_print_silent", True)
+
+        return opts
+
+
+class FirefoxDisableGeoLocationAbilitiesMixin:
+
+    def get_driver_options(self):
+        opts = super().get_driver_options()
+
+        opts.set_preference("geo.enabled", False)
+        opts.set_preference("geo.provider.use_corelocation", False)
+        opts.set_preference("geo.prompt.testing", False)
+        opts.set_preference("geo.prompt.testing.allow", False)
+
+        return opts
