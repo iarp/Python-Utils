1993645016d1f34638577373889f92a1ac7ee53c
iarp <iarp.opensource@gmail.com>
2025-05-31 16:57:30 -0400

    feat(crontabs): add bunch of generation functions

--- iarp_utils/crontabs.py
+++ iarp_utils/crontabs.py
@@ -1,3 +1,5 @@
+import datetime
+import random
 import re
 
 from .datetimes import weekday
@@ -153,3 +155,260 @@ def parse(crontab):
     day_of_week = CrontabParser(8).parse(day_of_week_raw)
 
     return minutes, hours, days_of_month, months_of_year, day_of_week
+
+
+def isoweekday_sunday_zero(isoweekday):
+    # Take datetime isoweekday value and convert it to crontab 0-6
+    return 0 if isoweekday == 7 else isoweekday
+
+
+def is_active_now(crontab, now):
+    minutes, hours, days_of_month, months_of_year, day_of_week = parse(crontab)
+
+    # print(f"{minutes=}")
+    # print(f"{hours=}")
+    # print(f"{days_of_month=}")
+    # print(f"{months_of_year=}")
+    # print(f"{day_of_week=}")
+
+    # minute calculation only works if this task runs and
+    # completes within the minute it's expected to run.
+    #   If it falls outside the expected minute, then the
+    #   channel will not get scanned.
+    if now.minute not in minutes:
+        return
+    if now.hour not in hours:
+        return
+    if now.day not in days_of_month:
+        return
+
+    # weekday and isoweekday returns variations of 1-7, crontab needs 0-6
+    if isoweekday_sunday_zero(now.isoweekday()) not in day_of_week:
+        return
+    if now.month not in months_of_year:
+        return
+
+    return True
+
+
+def calculate_schedule(crontab, check_month=False, period=10, now=None):
+    if not now:
+        now = datetime.datetime.now()
+    now = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    if check_month:
+        now = now.replace(day=1)
+    matched_timestamps = []
+    start_day = now.day
+    start_month = now.month
+    while True:
+        if is_active_now(crontab, now):
+            matched_timestamps.append(now)
+
+        now = now + datetime.timedelta(minutes=period)
+        if not check_month and now.day != start_day:
+            break
+        elif check_month and now.month != start_month:
+            break
+
+    return matched_timestamps
+
+
+def validate_crontab_values(minute=None, hour=None, day_of_week=None, day_of_month=None):
+    if isinstance(minute, int):
+        if not 0 <= minute <= 59:
+            raise ValueError(f"crontab minute must be between 0-59, was given {minute=}")
+        if minute % 10 != 0:
+            raise ValueError(f"crontab minute value must be divisible by 10, was given {minute=}")
+    if isinstance(hour, int) and not 0 <= hour <= 23:
+        raise ValueError(f"crontab hour must be between 0-23, was given {hour=}")
+    if isinstance(day_of_week, int) and not 0 <= day_of_week <= 6:
+        raise ValueError(f"crontab day_of_week must be between 0-6, was given {day_of_week=}")
+    if isinstance(day_of_month, int) and not 1 <= day_of_month <= 31:
+        raise ValueError(f"crontab day_of_month must be between 1-31, was given {day_of_month=}")
+    return True
+
+
+def generate_weekly(minute=None, hour=None, day_of_week=None):
+    """
+    day_of_week can be 0-7 with 0 and 7 meaning sunday.
+    """
+
+    minutes = list(range(0, 60, 10))
+    hours = list(range(8, 20))
+    month = "*"
+
+    if isinstance(hour, list):
+        hour = random.choice(hour)
+
+    minute = minute or random.choice(minutes)
+    hour = hour or random.choice(hours)
+    day_of_month = "*"
+
+    if not day_of_week:
+        day_of_week = random.choice(list(range(0, 7)))
+    elif isinstance(day_of_week, list):
+        day_of_week = random.choice(day_of_week)
+
+    validate_crontab_values(minute=minute, hour=hour, day_of_week=day_of_week)
+
+    return f"{minute} {hour} {day_of_month} {month} {day_of_week}"
+
+
+def generate_monthly(minute=None, hour=None, day=None):
+
+    if isinstance(hour, list):
+        hour = random.choice(hour)
+
+    minutes = list(range(0, 60, 10))
+    hours = list(range(8, 20))
+    days = list(range(1, 29))  # 29 ensures it'll run even in February
+    month = "*"
+
+    minute = minute or random.choice(minutes)
+    hour = hour or random.choice(hours)
+    day_of_month = day or random.choice(days)
+    day_of_week = "*"
+
+    validate_crontab_values(minute=minute, hour=hour, day_of_month=day)
+
+    return f"{minute} {hour} {day_of_month} {month} {day_of_week}"
+
+
+def generate_biyearly(minute=None, hour=None, day=None):
+
+    minutes = list(range(0, 60, 10))
+    hours = list(range(8, 20))
+    days = list(range(1, 29))  # 29 ensures it'll run even in February
+    month = list(range(1, 13))
+
+    if isinstance(hour, list):
+        hour = random.choice(hour)
+
+    minute = minute or random.choice(minutes)
+    hour = hour or random.choice(hours)
+    day_of_month = day or random.choice(days)
+    month1 = random.choice(month)
+    month2 = (month1 + 6) % 12 or 1
+    day_of_week = "*"
+
+    validate_crontab_values(minute=minute, hour=hour, day_of_month=day)
+
+    return f"{minute} {hour} {day_of_month} {month1},{month2} {day_of_week}"
+
+
+def generate_yearly(minute=None, hour=None, day=None):
+
+    minutes = list(range(0, 60, 10))
+    hours = list(range(8, 20))
+    days = list(range(1, 29))  # 29 ensures it'll run even in February
+    month = list(range(1, 13))
+
+    if isinstance(hour, list):
+        hour = random.choice(hour)
+
+    minute = minute or random.choice(minutes)
+    hour = hour or random.choice(hours)
+    day_of_month = day or random.choice(days)
+    month = random.choice(month)
+    day_of_week = "*"
+
+    validate_crontab_values(minute=minute, hour=hour)
+
+    return f"{minute} {hour} {day_of_month} {month} {day_of_week}"
+
+
+def generate_every_other_day(minute=None, hour=None):
+
+    minutes = list(range(0, 60, 10))
+    hours = list(range(8, 20))
+
+    if isinstance(hour, list):
+        hour = random.choice(hour)
+
+    minute = minute or random.choice(minutes)
+    hour = hour or random.choice(hours)
+    day_of_month = "*"
+    month = "*"
+    day_of_week = random.choice(["0-7/2", "1-7/2"])
+
+    validate_crontab_values(minute=minute, hour=hour)
+
+    return f"{minute} {hour} {day_of_month} {month} {day_of_week}"
+
+
+def generate_daily(minute=None, hour=None):
+
+    minutes = list(range(0, 60, 10))
+    hours = list(range(8, 20))
+
+    if isinstance(hour, list):
+        hour = random.choice(hour)
+
+    minute = minute or random.choice(minutes)
+    hour = hour or random.choice(hours)
+    day_of_month = "*"
+    month = "*"
+    day_of_week = "*"
+
+    validate_crontab_values(minute=minute, hour=hour)
+
+    return f"{minute} {hour} {day_of_month} {month} {day_of_week}"
+
+
+def generate_selection_monthly_crontabs(length=22, minute=0, hour=5, increment_minute=10, increment_hour=1):
+    vals = []
+
+    for x in range(length):
+
+        day_of_month = random.choice(range(1, 30))
+
+        vals.append(f"{minute} {hour} {day_of_month} * *")
+
+        minute += increment_minute
+        if minute >= 60:
+            minute = 0
+            hour += increment_hour
+
+    return vals
+
+
+def generate_selection_biweekly_crontabs(length=22, minute=0, hour=13, increment_minute=10, increment_hour=1):
+    vals = []
+
+    week_of_month_options = [
+        list(range(1, 14 + 1)),
+        list(range(14, 30 + 1)),
+    ]
+
+    for x in range(length):
+
+        day_of_week_selection_list = []
+        for opt in week_of_month_options:
+            check_day = random.choice(opt)
+            day_of_week_selection_list.append(str(check_day))
+
+        day_of_month = ",".join(day_of_week_selection_list)
+
+        vals.append(f"{minute} {hour} {day_of_month} * *")
+
+        minute += increment_minute
+        if minute >= 60:
+            minute = 0
+            hour += increment_hour
+
+    return vals
+
+
+def generate_selection_daily_crontabs(length=22, minute=0, hour=16, increment_minute=10, increment_hour=1):
+    vals = []
+
+    for x in range(length):
+
+        vals.append(f"{minute} {hour} * * *")
+
+        minute += increment_minute
+        if minute >= 60:
+            minute = 0
+            hour += increment_hour
+
+    return vals
--- tests/test_crontabs.py
+++ tests/test_crontabs.py
@@ -1,6 +1,6 @@
 import unittest
 
-from iarp_utils.crontabs import CrontabParser, ParseException, parse
+from iarp_utils.crontabs import *
 
 
 class CrontabParserTests(unittest.TestCase):
@@ -95,3 +95,177 @@ class CrontabParserTests(unittest.TestCase):
     def test_day_of_week_includes_zero_through_seven(self):
         *_, day_of_week = parse('* * * * *')
         self.assertEqual(day_of_week, {0, 1, 2, 3, 4, 5, 6, 7})
+
+    def test_day_of_week_as_text(self):
+        self.assertEqual(CrontabParser(7).parse('mon'), {1})
+        self.assertEqual(CrontabParser(7).parse('monday'), {1})
+        self.assertEqual(CrontabParser(7).parse('tues'), {2})
+        self.assertEqual(CrontabParser(7).parse('mon-fri'), {1, 2, 3, 4, 5})
+        with self.assertRaises(ValueError):
+            CrontabParser(7).parse('invalid')
+
+        m, h, d, month, day_of_week = parse('* * mon-fri * tues-thurs')
+        self.assertEqual(d, {1, 2, 3, 4, 5})
+        self.assertEqual(day_of_week, {2, 3, 4})
+
+    def test_weekday_by_name_into_number(self):
+        days = 'sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'
+        for index, day in zip(range(0, 6, 1), days):
+            self.assertEqual(index, weekday(day))
+
+        with self.assertRaises(KeyError):
+            weekday('weekday that does not exist')
+
+    def test_validate_crontab_values_minute(self):
+
+        self.assertTrue(validate_crontab_values(minute=0))
+        self.assertTrue(validate_crontab_values(minute=10))
+        self.assertTrue(validate_crontab_values(minute=20))
+        self.assertTrue(validate_crontab_values(minute=30))
+        self.assertTrue(validate_crontab_values(minute=40))
+        self.assertTrue(validate_crontab_values(minute=50))
+
+        def check_valueerror_raised(value):
+
+            for x in range(value, 60, 10):
+                with self.assertRaisesRegex(ValueError, 'divisible by 10'):
+                    validate_crontab_values(minute=x)
+
+        check_valueerror_raised(1)
+        check_valueerror_raised(2)
+        check_valueerror_raised(3)
+        check_valueerror_raised(4)
+        check_valueerror_raised(5)
+        check_valueerror_raised(6)
+        check_valueerror_raised(7)
+        check_valueerror_raised(8)
+        check_valueerror_raised(9)
+
+        with self.assertRaisesRegex(ValueError, 'between 0-59'):
+            validate_crontab_values(minute=60)
+
+    def test_validate_crontab_values_hour(self):
+        for x in range(0, 23, 1):
+            self.assertTrue(validate_crontab_values(hour=x))
+
+        with self.assertRaisesRegex(ValueError, 'between 0-23'):
+            validate_crontab_values(hour=24)
+
+    def test_validate_crontab_values_day_of_week(self):
+        for x in range(0, 6):
+            self.assertTrue(validate_crontab_values(day_of_week=x))
+
+        with self.assertRaisesRegex(ValueError, 'between 0-6'):
+            validate_crontab_values(day_of_week=7)
+
+    def test_validate_crontab_values_day_of_month(self):
+        for x in range(1, 31):
+            self.assertTrue(validate_crontab_values(day_of_month=x))
+
+        with self.assertRaisesRegex(ValueError, 'between 1-31'):
+            validate_crontab_values(day_of_month=0)
+        with self.assertRaisesRegex(ValueError, 'between 1-31'):
+            validate_crontab_values(day_of_month=32)
+
+    def test_validate_on_all_crontab_generator_functions(self):
+        with self.assertRaisesRegex(ValueError, 'divisible by 10'):
+            generate_daily(minute=12)
+        with self.assertRaisesRegex(ValueError, 'divisible by 10'):
+            generate_weekly(minute=12)
+        with self.assertRaisesRegex(ValueError, 'divisible by 10'):
+            generate_monthly(minute=12)
+        with self.assertRaisesRegex(ValueError, 'divisible by 10'):
+            generate_biyearly(minute=12)
+        with self.assertRaisesRegex(ValueError, 'divisible by 10'):
+            generate_yearly(minute=12)
+
+    def test_generate_daily(self):
+        output = generate_daily(minute=10, hour=2)
+        self.assertEqual('10 2 * * *', output)
+        output = generate_daily(minute=10, hour=[2,3])
+        self.assertRegex(output, r'^10 [2,3] \* \* \*$')
+
+    def test_generate_every_other_day(self):
+        output = generate_every_other_day(minute=10, hour=2)
+        self.assertRegex(output, r'^10 2 \* \* (0-7\/2|1-7\/2)$')
+        output = generate_every_other_day(minute=10, hour=[2,3])
+        self.assertRegex(output, r'^10 [2,3] \* \* (0-7\/2|1-7\/2)$')
+
+    def test_generate_weekly(self):
+        output = generate_weekly(minute=10, hour=2, day_of_week=6)
+        self.assertEqual('10 2 * * 6', output)
+
+        output = generate_weekly(minute=10, hour=[2, 3], day_of_week=6)
+        self.assertRegex(output, r'^10 [2,3] \* \* 6$')
+
+        output = generate_weekly(minute=10, hour=[2, 3], day_of_week=None)
+        self.assertRegex(output, r'^10 [2,3] \* \* [0-7]$')
+
+    def test_generate_weekly_multiple_days(self):
+        output = generate_weekly(minute=10, hour=[2, 3], day_of_week=[3,6])
+        self.assertRegex(output, r'^10 [2,3] \* \* [3,6]$')
+
+    def test_generate_monthly(self):
+        output = generate_monthly(minute=10, hour=2, day=4)
+        self.assertEqual('10 2 4 * *', output)
+
+        output = generate_monthly(minute=10, hour=[2, 3], day=4)
+        self.assertRegex(output, r'^10 [2,3] 4 \* \*$')
+
+    def test_generate_biyearly(self):
+        output = generate_biyearly(minute=10, hour=2, day=4)
+        self.assertRegex(output, r'^10 2 4 \d{1,2},\d{1,2} \*$')
+
+        output = generate_biyearly(minute=10, hour=[2, 3], day=4)
+        self.assertRegex(output, r'^10 [2,3] 4 \d{1,2},\d{1,2} \*$')
+
+    def test_generate_yearly(self):
+        output = generate_yearly(minute=10, hour=2, day=4)
+        self.assertRegex(output, r'^10 2 4 \d+ \*$')
+
+        output = generate_yearly(minute=10, hour=[2, 3], day=4)
+        self.assertRegex(output, r'^10 [2,3] 4 \d+ \*$')
+
+    def test_generate_selection_monthly_crontabs(self):
+        output = generate_selection_monthly_crontabs(length=8)
+        self.assertEqual(8, len(output))
+
+        re_days = "|".join(f"{x}" for x in range(32))
+
+        self.assertRegex(output[0], fr'0 5 [{re_days}] * *')
+        self.assertRegex(output[1], fr'10 5 [{re_days}] * *')
+        self.assertRegex(output[2], fr'20 5 [{re_days}] * *')
+        self.assertRegex(output[3], fr'30 5 [{re_days}] * *')
+        self.assertRegex(output[4], fr'40 5 [{re_days}] * *')
+        self.assertRegex(output[5], fr'50 5 [{re_days}] * *')
+        self.assertRegex(output[6], fr'0 6 [{re_days}] * *')
+        self.assertRegex(output[7], fr'10 6 [{re_days}] * *')
+
+    def test_generate_selection_biweekly_crontabs(self):
+        output = generate_selection_biweekly_crontabs(length=8)
+        self.assertEqual(8, len(output))
+
+        re_days_first = "|".join(f"{x}" for x in range(1, 15))
+        re_days_second = "|".join(f"{x}" for x in range(14, 31))
+
+        self.assertRegex(output[0], fr'0 13 [{re_days_first},{re_days_second}] * *')
+        self.assertRegex(output[1], fr'10 13 [{re_days_first},{re_days_second}] * *')
+        self.assertRegex(output[2], fr'20 13 [{re_days_first},{re_days_second}] * *')
+        self.assertRegex(output[3], fr'30 13 [{re_days_first},{re_days_second}] * *')
+        self.assertRegex(output[4], fr'40 13 [{re_days_first},{re_days_second}] * *')
+        self.assertRegex(output[5], fr'50 13 [{re_days_first},{re_days_second}] * *')
+        self.assertRegex(output[6], fr'0 14 [{re_days_first},{re_days_second}] * *')
+        self.assertRegex(output[7], fr'10 14 [{re_days_first},{re_days_second}] * *')
+
+    def test_generate_selection_daily_crontabs(self):
+        output = generate_selection_daily_crontabs(length=8)
+        self.assertEqual(8, len(output))
+
+        self.assertEqual('0 16 * * *', output[0])
+        self.assertEqual('10 16 * * *', output[1])
+        self.assertEqual('20 16 * * *', output[2])
+        self.assertEqual('30 16 * * *', output[3])
+        self.assertEqual('40 16 * * *', output[4])
+        self.assertEqual('50 16 * * *', output[5])
+        self.assertEqual('0 17 * * *', output[6])
+        self.assertEqual('10 17 * * *', output[7])
